49c53a85066fa94bd3afc93468975dea059968c0
passelle <pascal.selle@non.agilent.com>
2017-06-06 15:13:32 +0200

    improved error handling

--- src/conf/terra_conf_read_sched_clocks.c
+++ src/conf/terra_conf_read_sched_clocks.c
@@ -1,5 +1,9 @@
 #include "terra_conf.h"
 
+#define HANDLE_ERROR(s){		\
+	fprintf(stderr, "%s", s);	\
+	goto error;}
+
 #define CONF_SCHED_CLOCK_BEGIN "begin clock schedule"
 #define CONF_SCHED_CLOCK_END "end clock schedule"
 
@@ -26,36 +30,24 @@ BOOL terra_conf_read_sched_clocks(terra_conf * const conf, FILE * const f)
 			continue;
 
 		if ((read = getline(&line, &buf_len, f)) == -1)
-		{
-			fprintf(stderr, "unexpected end of clock schedule section\n");
-			goto error;
-		}
+			HANDLE_ERROR("unexpected end of clock schedule section\n");
 
 		if (strncmp(line, CONF_SCHED_CLOCK_NAME, sizeof(CONF_SCHED_CLOCK_NAME) - 1) == 0 && line[sizeof(CONF_SCHED_CLOCK_NAME) - 1] == '=')
 		{
 			strncpy(&(conf->sched_clocks[conf->sched_clocks_len].sched.name), line + sizeof(CONF_SCHED_CLOCK_NAME), read - sizeof(CONF_SCHED_CLOCK_NAME) - 1);
 		}
 		else
-		{
-			fprintf(stderr, "clock schedule name expected\n");
-			goto error;
-		}
+			HANDLE_ERROR("clock schedule name expected\n");
 
 		if ((read = getline(&line, &buf_len, f)) == -1)
-		{
-			fprintf(stderr, "unexpected end of clock schedule section\n");
-			goto error;
-		}
+			HANDLE_ERROR("unexpected end of clock schedule section\n");
 
 		if (strncmp(line, CONF_SCHED_CLOCK_SOCK, sizeof(CONF_SCHED_CLOCK_SOCK) - 1) == 0 && line[sizeof(CONF_SCHED_CLOCK_SOCK) - 1] == '=')
 		{
 			conf->sched_clocks[conf->sched_clocks_len].sched.sock = line[sizeof(CONF_SCHED_CLOCK_SOCK)];
 		}
 		else
-		{
-			fprintf(stderr, "clock schedule sock expected\n");
-			goto error;
-		}
+			HANDLE_ERROR("clock schedule sock expected\n");
 
 		conf->sched_clocks[conf->sched_clocks_len].sched.trig = TRIGGER_CLOCK;
 		conf->sched_clocks_len++;
