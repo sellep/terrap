9065391c9f48e4886886626040d4e5b5be5eb53c
passelle <pascal.selle@non.agilent.com>
2017-06-07 09:03:38 +0200

    added terra_log_info va_list

--- src/utils/terra_log.h
+++ src/utils/terra_log.h
@@ -2,9 +2,10 @@
 #define __P_TERRA_UTILS_LOG_H
 
 #include <syslog.h>
+#include <stdarg.h>
 
 #define TERRA_LOG_ID "terrad"
 
-void terra_log_info(char const * const);
+void terra_log_info(char const * const, va_list ap);
 
 #endif
--- src/utils/terra_log_info.c
+++ src/utils/terra_log_info.c
@@ -1,8 +1,8 @@
 #include "terra_log.h"
 
-void terra_log_info(char const * const msg)
+void terra_log_info(char const * const msg, va_list ap)
 {
 	openlog(TERRA_LOG_ID, LOG_PID, LOG_DAEMON);
-	syslog(LOG_INFO, msg);
+	syslog(LOG_INFO, msg, ap);
 	closelog();
 }
