95b9953d0d2fa78dd3936da9a25c1ad9968e13d1
passelle <pascal.selle@non.agilent.com>
2017-06-07 12:19:49 +0200

    improved scheduler

--- src/terrad_run_clock.c
+++ src/terrad_run_clock.c
@@ -4,11 +4,16 @@
 #include "utils/terra_time.h"
 
 static int _terra_clock_mode[TERRA_CONF_MAX_SCHED_CLOCKS];
+static size_t _terra_clock_ldiff_start[TERRA_CONF_MAX_SCHED_CLOCKS];
+static size_t _terra_clock_ldiff_end[TERRA_CONF_MAX_SCHED_CLOCKS];
 
 #define SOCK_CLOCK_NOT_ON(i)(_terra_clock_mode[i] != SOCK_ON)
 #define SOCK_CLOCK_NOT_OFF(i)(_terra_clock_mode[i] != SOCK_OFF)
 #define SOCK_CLOCK_SET_ON(i)(_terra_clock_mode[i] = SOCK_ON)
 #define SOCK_CLOCK_SET_OFF(i)(_terra_clock_mode[i] = SOCK_OFF)
+
+#define LAST_START_DIFF_SMALLER(i,diff)(_terra_clock_ldiff_start[i] < diff)
+#define LAST_END_DIFF_SMALLER(i,diff)(_terra_clock_ldiff_end[i] < diff)
 
 void terrad_run_clock_init(terra_conf const * const conf)
 {
@@ -17,6 +22,8 @@ void terrad_run_clock_init(terra_conf const * const conf)
 	for (i = 0; i < conf->sched_clocks_len; i++)
 	{
 		_terra_clock_mode[i] = SOCK_UNKNOWN;
+		_terra_clock_ldiff_start[i] = SIZE_MAX;
+		_terra_clock_ldiff_end[i] = SIZE_MAX;
 	}
 }
 
@@ -27,17 +34,21 @@ void terrad_run_clock(terra_sched_clock const * const clock, ssize_t const i, te
 	if (!clock->sched.enabled)
 		return;
 
-	diff = terra_time_diff(&sys_time, &clock->start);
-	if (diff == 0 && SOCK_CLOCK_NOT_ON(i))
+	diff = terra_time_diff(sys_time, &clock->start);
+	if (SOCK_CLOCK_NOT_ON(i) && (diff == 0 || LAST_START_DIFF_SMALLER(i, diff)))
 	{
 		SOCK_CLOCK_SET_ON(i);
 		terra_log_info("switch clock %s to on\n", clock->sched.name);
 	}
 
+	_terra_clock_ldiff_start[i] = diff;
+
 	diff = terra_time_diff(sys_time, &clock->end);
-	if (diff == 0 && SOCK_CLOCK_NOT_OFF(i))
+	if (SOCK_CLOCK_NOT_OFF(i) && (diff == 0 || LAST_END_DIFF_SMALLER(i, diff)))
 	{
 		SOCK_CLOCK_SET_OFF(i);
 		terra_log_info("switch clock %s to off\n", clock->sched.name);
 	}
+
+	_terra_clock_ldiff_end[i] = diff;
 }
