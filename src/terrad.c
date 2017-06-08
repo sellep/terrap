4bfa142a9b8b742cdd3eb982a2124a9cdb989950
pascal <p.selle@aw-systems.net>
2017-06-08 19:41:24 +0200

    breaking changes

--- src/terrad.c
+++ src/terrad.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		terra_log_error("insufficient argument count\n");
-		terrad_led_set(LED_PIN_ERR, TRUE);
+		terra_led_set(LED_PIN_ERR, TRUE);
 		return 1;
 	}
 
@@ -27,28 +27,28 @@ int main(int argc, char **argv)
 	if (!terra_lock_init())
 	{
 		terra_log_error("failed to initialize terra lock\n");
-		terrad_led_set(LED_PIN_ERR, TRUE);
+		terra_led_set(LED_PIN_ERR, TRUE);
 		return 1;
 	}
 
 	if (!terra_conf_read(&conf, argv[1]))
 	{
 		terra_log_error("failed to read config file\n");
-		terrad_led_set(LED_PIN_ERR, TRUE);
+		terra_led_set(LED_PIN_ERR, TRUE);
 		return 1;
 	}
 
 	terra_pin_set_output(conf.led_pin_warn);
 	terra_pin_set_output(conf.switch_pin);
 
-	terrad_led_set(LED_PIN_ERR, FALSE);
-	terrad_led_set(conf.led_pin_warn, FALSE);
+	terra_led_set(LED_PIN_ERR, FALSE);
+	terra_led_set(conf.led_pin_warn, FALSE);
 
 //run
 	run_result = terrad_run(&conf);
 	terra_log_info("terra daemon stopped\n");
 
-	terrad_led_set(LED_PIN_ERR, TRUE);
+	terra_led_set(LED_PIN_ERR, TRUE);
 
 	if (!run_result)
 		return 1;
