0d78c0809b31770e227b8c1d61bd4f5d08afe43e
passelle <pascal.selle@non.agilent.com>
2017-06-08 08:21:06 +0200

    added lock draft

--- src/terra_lock_hw.c
+++ src/terra_lock_hw.c
@@ -11,46 +11,74 @@
 
 #define LOCK_FILE "terra_lock"
 
-static int sm_mutex;
-static pthread_mutex_t *mutex;
+static int _sm_mutex;
+static pthread_mutex_t *_mutex;
+static pthread_mutexattr_t _mutex_attr;
 
-int init_sm()
+#define LOCK()(pthread_mutex_lock(_mutex))
+#define UNLOCK()(pthread_mutex_unlock(_mutex))
+
+int init_lock()
 {
-	sm_mutex = shm_open(LOCK_FILE, O_CREAT | O_RDWR | O_TRUNC, S_IRWXU | S_IRWXG);
-	if (sm_mutex < 0)
+	_sm_mutex = shm_open(LOCK_FILE, O_CREAT | O_RDWR | O_TRUNC, S_IRWXU | S_IRWXG);
+	if (_sm_mutex < 0)
 	{
 		fprintf(stderr, "failed to create shared memory object (%s)\n", strerror(errno));
 		return 0;
 	}
 
-	if (ftruncate(sm_mutex, sizeof(pthread_mutex_t)) == -1)
+	if (ftruncate(_sm_mutex, sizeof(pthread_mutex_t)) == -1)
 	{
 		fprintf(stderr, "failed to truncate shared memory object\n");
 		return 0;
 	}
 
-	mutex = (pthread_mutex_t*) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, sm_mutex, 0);
-	if (mutex == MAP_FAILED)
+	_mutex = (pthread_mutex_t*) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, _sm_mutex, 0);
+	if (_mutex == MAP_FAILED)
 	{
 		fprintf(stderr, "failed to map memory\n");
 		return 0;
 	}
+
+	pthread_mutexattr_init(&_mutex_attr);
+	pthread_mutexattr_setpshared(&_mutex_attr, PTHREAD_PROCESS_SHARED);
+	pthread_mutex_init(_mutex, &_mutex_attr);
+
 	return 1;
 }
 
-void cleanup_sm()
+void cleanup_lock()
 {
-	pthread_mutex_destroy(mutex);
+	pthread_mutexattr_destroy(&_mutex_attr);
+	pthread_mutex_destroy(_mutex);
 	shm_unlink(LOCK_FILE);
 }
 
 int main()
 {
-	if (!init_sm())
+	if (!init_lock())
 	{
 		exit(1);
 	}
+	
+	if (fork() > 0)
+	{
+		//i'm the parent process, so let me sleep!
+		sleep(10);
 
-	cleanup_sm();
+		LOCK();
+		printf("hello from parent!\n");
+		UNLOCK();
+
+		cleanup_lock();
+	}
+	else
+	{
+		LOCK();
+		printf("hello from child!\n");
+		sleep(5000);
+		UNLOCK();
+	}
+
 	return 0;
 }
