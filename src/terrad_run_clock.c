83d796ef9be9bbb711c13f271be7ffb69dd61a55
passelle <pascal.selle@non.agilent.com>
2017-06-09 10:53:53 +0200

    enhanced terrad_run_clock by support of times

--- src/terrad_run_clock.c
+++ src/terrad_run_clock.c
@@ -24,22 +24,39 @@ void terrad_run_clock_init(terra_conf const * const conf)
 
 void terrad_run_clock(terra_sched_clock const * const clock, ssize_t const i, terra_time const * const sys_time)
 {
+	terra_time *start;
+	terra_time *stop;
 	size_t diff;
+	size_t j;
 
 	if (!clock->sched.enabled)
 		return;
 
-	//diff = terra_time_diff(sys_time, &clock->start);
-	//if (SWITCH_NOT_ON(i) && (diff == 0 || TIME_BETWEEN(sys_time, &clock->start, &clock->end)))
-	//{
-	//	SWITCH_SET_ON(i);
-	//	terra_log_info("switch clock %s to on (diff: %us)\n", clock->sched.name, diff);
-	//}
-    //
-	//diff = terra_time_diff(sys_time, &clock->end);
-	//if (SWITCH_NOT_OFF(i) && (diff == 0 || terra_time_cmp(sys_time, &clock->end) == TIME_ABOVE))
-	//{
-	//	SWITCH_SET_OFF(i);
-	//	terra_log_info("switch clock %s to off (diff: %us)\n", clock->sched.name, diff);
-	//}
+	for (j = 0; j < clock->times_len; j++)
+	{
+		start = &(clock->times[j].start);
+		stop = &(clock->times[j].stop);
+
+		diff = terra_time_diff(sys_time, start);
+
+		if (diff == 0 || TIME_BETWEEN(sys_time, start, stop))
+		{
+			if (SWITCH_NOT_ON(i))
+			{
+				SWITCH_SET_ON(i);
+				terra_log_info("switch clock %s to on (diff: %us)\n", clock->sched.name, diff);
+			}
+
+			//nothing todo anymore for this schedule
+			return;
+		}
+	}
+
+	//here, switch should be off
+
+	if (SWITCH_NOT_OFF(i))
+	{
+		SWITCH_SET_OFF(i);
+		terra_log_info("switch clock %s to off (diff: %us)\n", clock->sched.name, diff);
+	}
 }
