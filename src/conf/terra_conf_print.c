4e3012e5dd5374eae434f27b2229eca2f990c3c8
passelle <pascal.selle@non.agilent.com>
2017-06-09 10:19:37 +0200

    enhanced sched clock

--- src/conf/terra_conf_print.c
+++ src/conf/terra_conf_print.c
@@ -31,7 +31,7 @@ void terra_conf_print(terra_conf const * const conf)
 		terra_log_info("[sched_clock %u] sock: %c\n", i, conf->sched_clocks[i].sched.sock);
 		terra_log_info("[sched_clock %u] enabled: %u\n", i, conf->sched_clocks[i].sched.enabled);
 
-		for (j = 0; j < &conf->sched_clocks[i]->times_len; j++)
+		for (j = 0; j < &conf->sched_clocks[i].times_len; j++)
 		{
 			terra_log_info("[sched_clock %u] start: ", i);
 			terra_time_print(&(conf->sched_clocks[i].times[j].start));
