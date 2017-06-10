7d084419773fee5b85aa8412affb2b7826b5c25c
Pascal Selle AW-SYSTEMS GmbH <p.selle@aw-systems.net>
2017-06-10 23:35:40 +0200

    refactoring

--- src/utils/terra_pin_set.c
+++ src/utils/terra_pin_set.c
@@ -1,18 +1,8 @@
 #include "terra_pin.h"
 
-#ifdef WIRINGPI
-
-#include <wiringPi.h>
-
-#endif
-
 void terra_pin_set_output(ssize_t const pin)
 {
-#ifdef WIRINGPI
 	LOCK();
-	pinMode(pin, OUTPUT);
+	pi_2_mmio_set_output(pin);
 	UNLOCK();
-#else
-	terra_log_info("[terra_pin_set_output] wiringPi disabled!\n");
-#endif
 }
