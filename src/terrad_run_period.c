3347e8df977c1fed50450edfd852b0aae7e75240
passelle <pascal.selle@non.agilent.com>
2017-06-12 09:12:08 +0200

    implemented terrad period logic

--- src/terrad_run_period.c
+++ src/terrad_run_period.c
@@ -1,6 +1,7 @@
 #include "terra.h"
 
 #include "conf/terra_conf.h"
+#include "switch/terra_switch.h"
 
 typedef struct
 {
@@ -13,13 +14,13 @@ static sched_period_cache sched_period_caches[TERRA_CONF_MAX_SCHED_PERIODS];
 inline void change_switch(
 	terra_conf const * const conf,
 	terra_sched_period const * const period,
-	sched_period_cache const * const cache,
+	sched_period_cache * const cache,
 	terra_time const * const sys_time,
 	switch_mode const mode)
 {
 	terra_switch_req req;
 
-	terra_time_cpy(&cache->begin, sys_time);
+	terra_time_cpy(&(cache->begin), sys_time);
 	cache->mode = mode;
 
 	req.sock = period->sched.sock;
@@ -35,12 +36,12 @@ void terrad_run_period_init(terra_conf const * const conf, terra_time const * co
 
 	for (i = 0; i < conf->sched_periods_len; i++)
 	{
-		period = &conf->sched_periods[i];
+		period = &(conf->sched_periods[i]);
 
 		if (SCHED_DISABLED(period))
 			continue;
 
-		cache = &sched_period_caches[i];
+		cache = &(sched_period_caches[i]);
 
 		change_switch(conf, period, cache, sys_time, SWITCH_ON);
 	}
@@ -55,23 +56,23 @@ BOOL terrad_run_period(terra_conf const * const conf, terra_time const * const s
 
 	for (i = 0; i < conf->sched_periods_len; i++)
 	{
-		period = &conf->sched_periods[i];
+		period = &(conf->sched_periods[i]);
 
 		if (SCHED_DISABLED(period))
 			continue;
 
-		cache = &sched_period_caches[i];
+		cache = &(sched_period_caches[i]);
 
-		terra_time_difft(&diff, &sys, &cache->begin);
+		terra_time_difft(&diff, &sys, &(cache->begin));
 
 		if (cache->mode == SWITCH_ON)
 		{
-			if (terra_time_cmp(&diff, &period->on_dur) != TIME_BELOW)
+			if (terra_time_cmp(&diff, &(period->on_dur)) != TIME_BELOW)
 			{
 				change_switch(conf, period, cache, sys_time, SWITCH_ON);
 			}
 		}
-		else if (terra_time_cmp(&diff, &period->off_dur) != TIME_BELOW)
+		else if (terra_time_cmp(&diff, &(period->off_dur)) != TIME_BELOW)
 		{
 			change_switch(conf, period, cache, sys_time, SWITCH_OFF);
 		}
