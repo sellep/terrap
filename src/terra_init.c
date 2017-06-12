d8a81221aa34ca1d8d21e2375e085a3302d702b1
passelle <pascal.selle@non.agilent.com>
2017-06-12 09:54:13 +0200

    fix config read

--- src/terra_init.c
+++ src/terra_init.c
@@ -8,19 +8,23 @@ BOOL terra_init()
 	mmio_status mstat;
 
 #ifndef DEBUG
+	printf("init mmio\n");
 	if ((mstat = pi_2_mmio_init()) != MMIO_SUCCESS)
 	{
 		terra_log_error("failed to initialize mmio\n");
 		return FALSE;
 	}
+#else
+	printf("skipping init mmio\n");
 #endif
 
+printf("skipping init lock\n");
 	if (!terra_lock_init())
 	{
 		terra_log_error("failed to initialize terra lock\n");
 		return FALSE;
 	}
-
+printf("terra_pin_set_output\n");
 	terra_pin_set_output(LED_PIN_ERR);
 	return TRUE;
 }
