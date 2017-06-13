cf715085f9a56f489708edd086c7a66b803e9290
Pascal <p.selle@aw-systems.net>
2017-06-13 14:54:43 +0200

    plot

--- res/terra.plot.sh
+++ res/terra.plot.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/gnuplot --persist
+#!/bin/bash
 
-set xdata time
-set timefmt '%H:%M:%S'
-set format x '%H:%M'
-set datafile sep '\t'
-set style data lines
-set terminal dumb
-
-plot "/tmp/hygro" using 1:3 title 'temp'
-
-#, '' using 1:3 title 'temperature'
+gnuplot terra.gnuplot.gp /tmp/hygro
