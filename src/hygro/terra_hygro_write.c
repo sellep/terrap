6da25d09037a873f2f1305ec65daf285a77a0547
passelle <pascal.selle@non.agilent.com>
2017-06-13 12:28:08 +0200

    added hygro write

--- src/hygro/terra_hygro_write.c
+++ src/hygro/terra_hygro_write.c
@@ -2,26 +2,26 @@
 
 #include <time.h>
 
-#define TIME struct tm
+#define DATE struct tm
 #define FORMAT_PATH "/var/opt/terra/hygro_%zu.%zu.%zu"
 
 static terra_time _last_write = { 0, 0, 0 };
 
-inline static TIME sys_time()
+inline static DATE sys_date()
 {
 	time_t t = time(NULL);
 	return *localtime(&t);
 }
 
-static BOOL terra_hygro_fwrite(float const h, float const f)
+static BOOL terra_hygro_fwrite(float const h, float const t)
 {
 	static char buf[40];
 
-	TIME syst;
+	DATE d;
 	FILE *f;
 
-	syst = sys_time();
-	sprintf(buf, FORMAT_PATH, syst.tm_tmday, syst.tm_mon + 1, syst.tm_year);
+	d = sys_date();
+	sprintf(buf, FORMAT_PATH, d.tm_tmday, d.tm_mon + 1, d.tm_year);
 
 	f = fopen(buf, "a+");
 	if (!f)
