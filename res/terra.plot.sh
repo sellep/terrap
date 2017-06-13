41e8d5e7d472755488f4d5463da9977b9b741336
Pascal <p.selle@aw-systems.net>
2017-06-13 14:40:47 +0200

    plot

--- res/terra.plot.sh
+++ res/terra.plot.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/gnuplot
+#!/usr/bin/gnuplot --persist
 
-set timefmt "%H:%M:%S"
+set timefmt '%H:%M:%S'
 set xdata time
 set datafile sep '\t'
 set style data lines
 set terminal dumb
-plot ARG1 using 1:2 title 'humidity', '' using 1:3 title 'temperature'
+
+plot "/tmp/hygro" using 1:2 title 'humidity', '' using 1:3 title 'temperature'
