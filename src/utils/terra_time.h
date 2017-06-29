5931314ead560b5de9f25964e3a95d1e93cb8e0d
passelle <pascal.selle@non.agilent.com>
2017-06-29 08:54:47 +0200

    build fix

--- src/utils/terra_time.h
+++ src/utils/terra_time.h
@@ -47,13 +47,8 @@ void busy_wait_milliseconds(size_t const);
 void terra_time_cpy(terra_time * const, terra_time const * const);
 void terra_time_difft(terra_time * const, terra_time const * const, terra_time const * const);
 
-static terra_time _day_end = { 23, 59, 59 };
-static terra_time _day_begin = { 0, 0, 0 };
-
 static inline BOOL terra_time_between(terra_time const * const tim, terra_time const * const begin, terra_time const * const end)
 {
-	//undefined behavior for begin equals end
-
 	if (terra_time_cmp(begin, end) == TIME_BELOW)
 	{
 		if (terra_time_cmp(tim, begin) != TIME_ABOVE)
