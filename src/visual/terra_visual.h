ebddf83bf65a0c62886a32e96fc81c09710569dd
passelle <pascal.selle@non.agilent.com>
2017-06-30 10:55:34 +0200

    added default visual show file

--- src/visual/terra_visual.h
+++ src/visual/terra_visual.h
@@ -55,7 +55,7 @@ typedef ssize_t terra_visual_cmd;
 
 inline static BOOL terra_visual_arg(terra_visual_cmd * const cmd, ssize_t const argc, char const * const * const argv)
 {
-	if (argc != 2)
+	if (argc == 2)
 	{
 		cmd[0] = 0;
 		return TRUE;
