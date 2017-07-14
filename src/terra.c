1c0a2da3471a299c58e82385da35c9a34714a1cc
passelle <pascal.selle@non.agilent.com>
2017-07-14 12:55:44 +0200

    architectural redesign

--- src/terra.c
+++ src/terra.c
@@ -44,10 +44,10 @@ int main(int argc, char ** argv)
 	}
 	else if (strcmp(argv[1], ARG_MODE_HYGRO) == 0)
 	{
-		if (!terra_hygro_run(&humi, &temp))
+		if (!terra_hygro_run(&humi, &temp, &CONF_HYGRO))
 			return 1;
 
-		terra_log_info("temperature %.2f, humidity: %.2f\n", temp, humi);
+		terra_log_info("humidity %.1f, temperature: %.1f\n", humi, temp);
 	}
 	else if (strcmp(argv[1], ARG_MODE_VISUAL) == 0)
 	{
