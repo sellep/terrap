70844637a3b3ab9b9b8238d1c78990e6718354de
passelle <pascal.selle@non.agilent.com>
2017-07-14 07:22:09 +0200

    enhanced visuals

--- src/visual/terra_visual_show.c
+++ src/visual/terra_visual_show.c
@@ -47,25 +47,25 @@ void terra_show(char const * const title, terra_data_entry const * const entries
 
 	setlocale(LC_CTYPE, "en_US.UTF-8");
 
+	initscr();
+	noecho();
+	curs_set(FALSE);
+
+	start_color();
+	init_pair(1, COLOR_DEFAULT, COLOR_BLACK);
+	init_pair(2, COLOR_GRID, COLOR_BLACK);
+	init_pair(3, COLOR_LEGEND, COLOR_BLACK);
+	init_pair(4, COLOR_TEMP, COLOR_BLACK);
+	init_pair(5, COLOR_HUMI, COLOR_BLACK);
+	init_pair(6, COLOR_BOTH, COLOR_BLACK);
+
+	getmaxyx(stdscr, height, width);
+	terra_visual_draw_title(title, width, count);
 
 	while (1)
 	{
-		initscr();
-		noecho();
-		curs_set(FALSE);
-
-		start_color();
-		init_pair(1, COLOR_DEFAULT, COLOR_BLACK);
-		init_pair(2, COLOR_GRID, COLOR_BLACK);
-		init_pair(3, COLOR_LEGEND, COLOR_BLACK);
-		init_pair(4, COLOR_TEMP, COLOR_BLACK);
-		init_pair(5, COLOR_HUMI, COLOR_BLACK);
-		init_pair(6, COLOR_BOTH, COLOR_BLACK);
-
-		getmaxyx(stdscr, height, width);
 		terra_visual_grid_init(&grid, width, height, entries, count, mode);
 
-		terra_visual_draw_title(title, width, count);
 		terra_visual_draw_grid(&grid, width, height);
 		terra_visual_draw_labels(width, height, &grid);
 
@@ -77,8 +77,9 @@ void terra_show(char const * const title, terra_data_entry const * const entries
 			break;
 
 		show_shift_mode(&mode);
-		endwin();
 	}
+
+	endwin();
 }
 
 #endif
