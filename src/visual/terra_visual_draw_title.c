82fc5f8d8293da1388d5397688f4fcec6c35b217
passelle <pascal.selle@non.agilent.com>
2017-07-17 09:47:11 +0200

    improved drawing

--- src/visual/terra_visual_draw_title.c
+++ src/visual/terra_visual_draw_title.c
@@ -2,48 +2,40 @@
 
 #define DETAILS_FORMAT "mode: %s, entries: %zu, temp: %.2f - %.2f, humi: %.2f - %.2f"
 
+#define METADATA_FORMAT_BOTH "mode: BOTH, entries: %zu, humi: %.1f - %.1f, temp: %.1f - %.1f"
+#define METADATA_FORMAT_HUMI "mode: HUMI, entries: %zu, humi: %.1f - %.1f"
+#define METADATA_FORMAT_TEMP "mode: TEMP, entries: %zu, temp: %.1f - %.1f"
+
+static char buf[256];
+
 static inline void visual_draw_metadata(terra_visual_grid const * const grid, size_t const len, terra_visual_mode const mode)
 {
 	if (mode == TERRA_BOTH)
 	{
-		mvprintw(1, 0, "mode: BOTH, entries: %zu, humi: %.1f - %.1f, temp: %.1f - %.1f", len, grid->min_humi, grid->max_humi, grid->min_temp, grid->max_temp);
+		sprintf(buf, METADATA_FORMAT_BOTH, len, grid->min_humi, grid->max_humi, grid->min_temp, grid->max_temp);
 	}
 	else if (mode == TERRA_HUMI)
 	{
-		mvprintw(1, 0, "mode: HUMI, entries: %zu, humi: %.1f - %.1f", len, grid->min_humi, grid->max_humi);
+		sprintf(buf, METADATA_FORMAT_HUMI, len, grid->min_humi, grid->max_humi);
 	}
 	else
 	{
-		mvprintw(1, 0, "mode: TEMP, entries: %zu, temp: %.1f - %.1f", len, grid->min_temp, grid->max_temp);
+		sprintf(buf, METADATA_FORMAT_TEMP, len, grid->min_temp, grid->max_temp);
 	}
+
+	mvprintw(1, 0, buf, strlen(buf));
 }
 
 void terra_visual_draw_title(terra_visual_grid const * const grid, char const * const title, ssize_t const width, size_t const entries, terra_visual_mode const mode)
 {
 #ifdef NCURSES
-	ssize_t tlen;
 	ssize_t len;
-	ssize_t i, j;
 
-	tlen = strlen(title);
 	len = (width - tlen) / 2;
 
-	for (i = 0, j = tlen; i < len; i++, j++)
-	{
-		printw("-");
-	}
-
-	printw(title);
-
-	for (i = 0; i < len; i++, j++)
-	{
-		printw("-");
-	}
-
-	if (j < width)
-	{
-		printw("-");
-	}
+	memset(buf, '-', sizeof buf);
+	strcpy(buf + len, title);
+	mvprintw(0, 0, buf, width);
 
 	visual_draw_metadata(grid, entries, mode);
 #endif
