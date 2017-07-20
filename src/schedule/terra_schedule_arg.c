5c52033076d6192124df9244f608f59e68a7592f
passelle <pascal.selle@non.agilent.com>
2017-07-20 10:30:49 +0200

    improved daemon

--- src/schedule/terra_schedule_arg.c
+++ src/schedule/terra_schedule_arg.c
@@ -4,6 +4,12 @@
 
 void terra_schedule_arg(int const argc, char const * const * const argv)
 {
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("arg%i: %s\n", i, argv[i]);
+	}
+	return;
 	if (argc >= 3 && strcmp(argv[2], ARG_SCHEDULE_LOGFILE) == 0)
 	{
 		terra_log_use_file();
