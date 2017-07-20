c1cc008025ffe8673596c5e65114066f0b5e37a5
passelle <pascal.selle@non.agilent.com>
2017-07-20 08:47:55 +0200

    extracted signal handling

--- src/schedule/terra_schedule_run.c
+++ src/schedule/terra_schedule_run.c
@@ -1,6 +1,6 @@
 #include "terra_schedule.h"
 
-#include "utils/terra_signal.h"
+#include "../utils/terra_signal.h"
 
 #define DO_HEART_BEAT() if (runtime.tick % CONF_HEART.tick == 0) terra_heart_beat()
 #define DO_HYGRO_READ() (CONF_HYGRO.enabled && !hygro_wait(&CONF_HYGRO, &NOW))
