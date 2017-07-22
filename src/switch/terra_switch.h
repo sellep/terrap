f68f7f28dc5d5155b3743842addd7119305dac24
sellep <p.selle@aw-systems.net>
2017-07-22 08:34:50 +0000

    schedule channel conf

--- src/switch/terra_switch.h
+++ src/switch/terra_switch.h
@@ -18,57 +18,79 @@ typedef int terra_switch_mode;
 typedef struct
 {
 	char socket;
+	int channel;
 	terra_switch_mode mode;
 } terra_switch_req;
 
 extern BOOL terra_switch_arg(terra_switch_req * const, int const, char * * const);
 extern void terra_switch_transmit(terra_conf_switch const * const, int const);
 
-static inline void terra_switch_set_on(terra_conf_switch const * const conf, char const socket, char const * const sender)
+static inline terra_switch_group* terra_switch_group_get(terra_conf_switch const * const conf, int const channel)
 {
+	size_t i;
+
+	if (channel == 0)
+		return &conf->groups[0];
+
+	for (i = 0; i < conf->group_len; i++)
+	{
+		if (conf->groups[i].channel == channel)
+			return &conf->groups[i];
+	}
+
+	terra_log_error("[terra_switch_group_get] group with channel %i not found\n", channel);
+	return NULL;
+}
+
+static inline void terra_switch_set_on(terra_conf_switch const * const conf, char const socket, int const channel, char const * const sender)
+{
+	terra_switch_group *group = terra_switch_group_get(channel);
+
 	if (socket == 'a')
 	{
-		terra_switch_transmit(conf, conf->groups[0].code_aon);
+		terra_switch_transmit(conf, group->code_aon);
 	}
 	else if (socket == 'b')
 	{
-		terra_switch_transmit(conf, conf->groups[0].code_bon);
+		terra_switch_transmit(conf, group->code_bon);
 	}
 	else
 	{
-		terra_switch_transmit(conf, conf->groups[0].code_con);
+		terra_switch_transmit(conf, group->code_con);
 	}
 
-	terra_log_info("[terra_switch] set switch %c to on (%s)\n", socket, sender);
+	terra_log_info("[terra_switch] set switch %c:%i to on (%s)\n", socket, channel, sender);
 }
 
-static inline void terra_switch_set_off(terra_conf_switch const * const conf, char const socket, char const * const sender)
+static inline void terra_switch_set_off(terra_conf_switch const * const conf, char const socket, int const channel, char const * const sender)
 {
+	terra_switch_group *group = terra_switch_group_get(channel);
+
 	if (socket == 'a')
 	{
-		terra_switch_transmit(conf, conf->groups[0].code_aoff);
+		terra_switch_transmit(conf, group->code_aoff);
 	}
 	else if (socket == 'b')
 	{
-		terra_switch_transmit(conf, conf->groups[0].code_boff);
+		terra_switch_transmit(conf, group->code_boff);
 	}
 	else
 	{
-		terra_switch_transmit(conf, conf->groups[0].code_coff);
+		terra_switch_transmit(conf, group->code_coff);
 	}
 
-	terra_log_info("[terra_switch] set switch %c to off (%s)\n", socket, sender);
+	terra_log_info("[terra_switch] set switch %c:%i to off (%s)\n", socket, channel, sender);
 }
 
-static inline void terra_switch_set(terra_conf_switch const * const conf, char const socket, terra_switch_mode const mode, char const * const sender)
+static inline void terra_switch_set(terra_conf_switch const * const conf, char const socket, int const channel, terra_switch_mode const mode, char const * const sender)
 {
 	if (mode == SWITCH_ON)
 	{
-		terra_switch_set_on(conf, socket, sender);
+		terra_switch_set_on(conf, socket, channel, sender);
 	}
 	else if (mode == SWITCH_OFF)
 	{
-		terra_switch_set_off(conf, socket, sender);
+		terra_switch_set_off(conf, socket, channel, sender);
 	}
 }
 
