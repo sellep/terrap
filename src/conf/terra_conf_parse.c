dec1d4fbb2e9372099443a2574401147818ad58c
passelle <pascal.selle@non.agilent.com>
2017-07-31 13:20:58 +0200

    redesign period schedule

--- src/conf/terra_conf_parse.c
+++ src/conf/terra_conf_parse.c
@@ -132,13 +132,13 @@ terra_parse_result terra_conf_parse(terra_conf * const conf, char const * const 
 	switch_parse(conf, &lib);
 	hygro_parse(conf, &lib);
 
-	/*if (terra_conf_parse_schedule_clock(&conf->clocks, &conf->clock_len, &lib) != CONFIG_PARSE_OK)
+	if (terra_conf_parse_schedule_clock(&conf->clocks, &conf->clock_len, &lib) != CONFIG_PARSE_OK)
 	{
 		terra_log_error("[terra_conf_parse] failed to parse clock schedules\n");
 		goto exit;
 	}
 
-	if (terra_conf_parse_schedule_hygro(&conf->hygros, &conf->hygro_len, &lib) != CONFIG_PARSE_OK)
+	/*if (terra_conf_parse_schedule_hygro(&conf->hygros, &conf->hygro_len, &lib) != CONFIG_PARSE_OK)
 	{
 		terra_log_error("[terra_conf_parse] failed to parse hygro schedules\n");
 		goto exit;
