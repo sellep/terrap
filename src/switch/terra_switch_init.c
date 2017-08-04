322836108807d412c92d785c6d7205cbc8bd44c3
passelle <pascal.selle@non.agilent.com>
2017-08-04 11:11:42 +0200

    redesign switch

--- src/switch/terra_switch_init.c
+++ src/switch/terra_switch_init.c
@@ -19,7 +19,7 @@ BOOL terra_switch_init(terra_switch_mode * * const modes, terra_conf_switch cons
 		return FALSE;
 	}
 
-	modes[0][0] = SWITCH_UNKNWON;
+	modes[0][0] = SWITCH_UNKNOWN;
 	modes[0][1] = SWITCH_ON;
 	modes[0][2] = SWITCH_OFF;
 
