eb522a42a9c38435dea1e5a1efdf34a8d7b349b9
passelle <pascal.selle@non.agilent.com>
2017-08-04 11:52:09 +0200

    redesign switch

--- src/switch/terra_switch_init.c
+++ src/switch/terra_switch_init.c
@@ -6,13 +6,13 @@ static inline BOOL switch_open_file()
 {
 	BOOL do_truncate = TRUE;
 
-	file = shm_open(SWITCH_FILE, O_EXCL | O_CREAT | O_TRUNC | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
+	file = shm_open(SWITCH_FILE, O_EXCL | O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
 
 	if (errno == EEXIST)
 	{
 		do_truncate = FALSE;
 
-		file = shm_open(SWITCH_FILE, O_CREAT | O_TRUNC | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
+		file = shm_open(SWITCH_FILE, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
 	}
 
 	if (file < 0)
@@ -23,8 +23,6 @@ static inline BOOL switch_open_file()
 
 	if (do_truncate)
 	{
-		printf("do truncate\n");
-
 		if (ftruncate(file, sizeof(terra_switch_mode) * 3) == -1)
 		{
 			terra_log_error("[terra_switch_init] failed to truncate shared file (%s)\n", strerror(errno));
@@ -43,15 +41,13 @@ BOOL terra_switch_init(terra_switch_mode * * const modes, terra_conf_switch cons
 		return FALSE;
 	}
 
-	printf("initialized\n");
+	modes[0] = mmap(NULL, sizeof(terra_switch_mode) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
 
-	if ((modes[0] = mmap(NULL, sizeof(terra_switch_mode) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0)) == MAP_FAILED)
+	if (modes[0] == MAP_FAILED)
 	{
 		terra_log_error("[terra_switch_init] failed to map shared file (%s)\n", strerror(errno));
 		return FALSE;
 	}
-
-	printf("mapped\n");
 
 	return TRUE;
 }
