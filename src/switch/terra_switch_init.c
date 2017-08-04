fab1b9b61734df0ebdd1ae72be0f92e97ad0cb03
passelle <pascal.selle@non.agilent.com>
2017-08-04 11:25:11 +0200

    redesign switch

--- src/switch/terra_switch_init.c
+++ src/switch/terra_switch_init.c
@@ -2,24 +2,46 @@
 
 static int file;
 
-BOOL terra_switch_init(terra_switch_mode * * const modes, terra_conf_switch const * const conf)
+static inline BOOL switch_open_file()
 {
-	file = shm_open(SWITCH_FILE, O_CREAT | O_TRUNC | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
+	BOOL do_truncate = TRUE;
+
+	file = shm_open(SWITCH_FILE, O_EXCL | O_CREAT | O_TRUNC | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
+
+	if (errno == EEXIST)
+	{
+		do_truncate = FALSE;
+
+		file = shm_open(SWITCH_FILE, O_CREAT | O_TRUNC | O_RDWR, S_IRWXU | S_IRWXG | S_IROTH);
+	}
+
 	if (file < 0)
 	{
 		terra_log_error("[terra_switch_init] failed to create shared file (%s)\n", strerror(errno));
-		file = NULL;
 		return FALSE;
 	}
 
-	if (ftruncate(file, sizeof(terra_switch_mode) * 3) == -1)
+	if (do_truncate)
 	{
-		terra_log_error("[terra_switch_init] failed to truncate shared file (%s)\n", strerror(errno));
+		if (ftruncate(file, sizeof(terra_switch_mode) * 3) == -1)
+		{
+			terra_log_error("[terra_switch_init] failed to truncate shared file (%s)\n", strerror(errno));
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+BOOL terra_switch_init(terra_switch_mode * * const modes, terra_conf_switch const * const conf)
+{
+	if (!switch_open_file())
+	{
+		terra_log_error("[terra_switch_init] failed to create shared file (%s)\n", strerror(errno));
 		return FALSE;
 	}
 
-	modes[0] = mmap(NULL, sizeof(terra_switch_mode) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
-	if (modes[0] == MAP_FAILED)
+	if ((modes[0] = mmap(NULL, sizeof(terra_switch_mode) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0)) == MAP_FAILED)
 	{
 		terra_log_error("[terra_switch_init] failed to map shared file (%s)\n", strerror(errno));
 		return FALSE;
