32a41c4539d762af63247ed73cf5a570caae330d
passelle <pascal.selle@non.agilent.com>
2017-08-04 08:13:09 +0200

    shared switch modes

--- src/switch/terra_switch_init.c
+++ src/switch/terra_switch_init.c
@@ -12,7 +12,7 @@ BOOL terra_switch_init(terra_switch_mode * * const modes, terra_conf_switch cons
 		return FALSE;
 	}
 
-	modes[0] = mmap(NULL, sizeof(terra_switch_modes) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
+	modes[0] = mmap(NULL, sizeof(terra_switch_mode) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
 	if (modes[0] == MAP_FAILED)
 	{
 		terra_log_error("[terra_switch_init] failed to map shared file (%s)\n", strerror(errno));
